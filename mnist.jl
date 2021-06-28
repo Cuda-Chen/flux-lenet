579d9a0f48ea8f86f398cadc82567d6d7df6a10f
Cuda-Chen <clh960524@gmail.com>
2021-06-28 15:24:50 +0800

    update LeNet structure

--- mnist.jl
+++ mnist.jl
@@ -1,23 +1,24 @@
 using Flux
 
-function build_model(args; imgsize = (28, 28, 1), nclass = 10)
-    cnn_output_size = Int.(floor.(imgsize[1]/8,imgsize[2]/8,32))
+function build_model(args; imgsize = (28, 28, 1), nclasses = 10)
+    out_conv_size = (imgsize[1]÷4 - 3, imgsize[2]÷4 - 3, 16)
 
     return Chain(
     # First convolution, operating upon 28x28 image
-    Conv((3, 3), imgsize[3]=>32, pad=(1,1), relu),
-    MaxPool((2,2)),
+    Conv((5, 5), imgsize[end]=>6, relu),
+    MaxPool((2, 2)),
 
     # second convolution, operating upon 14x14 image
-    Conv((3,3), 16=>32, pad=(1,1), relu),
-    MaxPool((2,2)),
+    Conv((5, 5), 6=>16, relu),
+    MaxPool((2, 2)),
 
     # Third convolution, operating upon 7x7 image
-    Conv((3,3), 32=>32, pad=(1,1), relu),
-    MaxPool((2,2)),
+    #Conv((3,3), 32=>32, pad=(1,1), relu),
+    #MaxPool((2,2)),
 
-    # Reshape 3d tensor into a 2d one using `Flux.flatten`, at this point it should be (3, 3, 32, N)
     flatten,
-    Dense(prod(cnn_output_size), 10)
+    Dense(prod(out_conv_size), 120, relu),
+    Dense(120, 84, relu),
+    Dense(84, nclasses)
     )
 end
